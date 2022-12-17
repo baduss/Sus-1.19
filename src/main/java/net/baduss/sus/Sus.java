50cb21994c525f7ffc8dbc09fc2175f4bf712402
BuildTools <unconfigured@null.spigotmc.org>
2022-12-17 20:08:59 +0100

    Initial commit

---
+++ src/main/java/net/baduss/sus/Sus.java
@@ -0,0 +1,14 @@
+package net.baduss.sus;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Sus implements ModInitializer {
+	public static final String MOD_ID = "sus";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/baduss/sus/SusClient.java
@@ -0,0 +1,10 @@
+package net.baduss.sus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
